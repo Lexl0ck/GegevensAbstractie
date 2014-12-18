b3667bc85838657d9404d32c8de438c52304a009
JeroenVerstraelen <jeroenverstraelen@hotmail.com>
2014-12-18 21:50:17 +0100

    changelog

--- Main (python code)/movietheatre.py
+++ Main (python code)/movietheatre.py
@@ -17,7 +17,7 @@ class Movietheatre:
 
         # Tables (Showing and Film)
         self.showing_table = Table()
-        self.showing_table.setImplementation("DoubleLinkedChain")
+        self.showing_table.setImplementation("doublylinkedchain")
         self.showing_table.createTable()
 
         self.film_table = Table()
