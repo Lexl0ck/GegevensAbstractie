d5f065ccf7ad32a80cd874ce3ed2fc299be060f6
Stein de Groof <steindegroof@gmail.com>
2014-12-19 15:49:25 +0100

    refactored double_linked_chain

--- Main (python code)/doubly_linked_chain.py
+++ Main (python code)/doubly_linked_chain.py
@@ -7,98 +7,72 @@ class Doubly_linked_chain:
             A new node is created by giving it an an item and the two links.
             Example: node(previous Node, item, next node)
             The searchkey for a node is its item if its a single value or its first object if it's a list.'''
-        def __init__(self,prevNode,item,nextNode, searchKey = 0):
+        def __init__(self, prevNode, nextNode, searchKey = None, item = None):
             self.next = nextNode
             self.prev = prevNode
+            if item == None:
+                item = searchKey
             self.item = item
-            if searchKey == 0:
-                self.searchKey = item
-                if type(item) == type([]):
-                    self.searchKey = item[0]
-            else:
-                self.searchKey = searchKey
-
+            self.searchKey = searchKey
                 
     
     def __init__(self):
-        dummy_head_node = self.node(None,None,None) #a dummy head Node to prevent the special case of inserting at the head.
+        dummy_head_node = self.node(None,None) #a dummy head Node to prevent the special case of inserting at the head.
+        dummy_head_node.next = dummy_head_node
+        dummy_head_node.prev = dummy_head_node
         self.headPtr = dummy_head_node
-        self.tailPtr = dummy_head_node
         self.length = 0
 
-    def __reset_tailPtr(self):
-        ''' Sets the tail pointer to the last Position again in case something is added or removed from the tail of the chain. '''
-        def isLastNode(node):
-            if node.next == None:
-                return node
-        self.tailPtr = self.traverse(isLastNode)
-            
-
-    def insert(self,item, searchkey=0):
+    def insert(self, searchkey, item = None):
         ''' Adds an item to the front of the chain after the dummy head node.'''
         dummyHeadNode = self.headPtr
         nextNode = dummyHeadNode.next
-        tmpNode = self.node(dummyHeadNode,item,dummyHeadNode.next,searchkey)
-        dummyHeadNode.next = tmpNode
-        if nextNode != None:
-            nextNode.prev = tmpNode
+        if searchkey == None:
+            return false
+        newNode = self.node(dummyHeadNode, dummyHeadNode.next, searchkey, item)
+        dummyHeadNode.next = newNode
+        nextNode.prev = newNode
         self.length +=1
-        self.__reset_tailPtr()
 
-
-    def traverse(self,visit):
-        ''' Traverses the chain from the head to the tail calling visit(current node) at each node.
-            If visit is a fruitfull function the traversal will be stopped when another value than
-            None is returned and the by visit returned value will be returned by traverse().
-        '''
-        tmpNode = self.headPtr
-        tmpNode = tmpNode.next #skip the dummy head node
-        while tmpNode != None:
-            tmp = visit(tmpNode)
-            if tmp != None:
-                return tmp
-            tmpNode = tmpNode.next
 
     def remove(self,searchKey):
         ''' Removes the first node with the given searchkey from the chain.
             The method returns True if it was successfull or False if not.
         '''
-        tmp = self.getNode(searchKey)
-        
-        if tmp == None:
+        node = self.getNode(searchKey)
+        if not node:
             return False
-        prevNode = tmp.prev
-        nextNode = tmp.next
+        prevNode = node.prev
+        nextNode = node.next
         prevNode.next = nextNode
-        if nextNode != None:
-            nextNode.prev = prevNode
+        nextNode.prev = prevNode
         self.length -=1
-        self.__reset_tailPtr()
         return True
 
 
     def clear(self):
         ''' Removes all the nodes from the chain except for the dummy head node. '''
         dummyHeadNode = self.headPtr
-        dummyHeadNode.next = None
-        self.tailPtr = dummyHeadNode
+        dummyHeadNode.next = dummyHeadNode
+        dummyHeadNode.prev = dummyHeadNode
         
 
     def getNode(self,searchKey):
         ''' Returns the first node found with the given searchkey.'''
-        def compare(node):
-            if node.searchKey == searchKey:
-                return node
-        return self.traverse(compare)
+        if searchKey == None:
+            return False
+        tmpNode = self.headPtr.next
+        while tmpNode.searchKey != None:
+            if tmpNode.searchKey == searchKey:
+                return tmpNode
+            tmpNode = tmpNode.next
+        return False
 
     def getItem(self, searchKey):
         ''' Returns the first item found with the given searchkey. '''
-        tmpNode = self.headPtr
-        tmpNode = tmpNode.next #skip the dummy head node
-        while tmpNode != None:
-            if tmpNode.searchKey == searchKey:
-                return tmpNode.searchKey
-            tmpNode = tmpNode.next
+        node = self.getNode(searchKey)
+        if node:
+            return node.item
         return False
 
     def getLength(self):
@@ -106,17 +80,31 @@ class Doubly_linked_chain:
 
     def isEmpty(self):
         ''' Returns True if the chain is empty or false if not. '''
-        if self.tailPtr is None:
+        if self.headPtr == self.headPtr.next:
             return True
         return False
 
-    def getTraverse(self):
+    def traverse(self):
         ''' returns a list of all the items in the chain '''
         tmpList = []
-        tmpNode = self.headPtr
-        tmpNode = tmpNode.next #skip the dummy head node
-        while tmpNode != None:
+        tmpNode = self.headPtr.next
+        while tmpNode.searchKey != None:
             tmpList.append(tmpNode.searchKey)
             tmpNode = tmpNode.next
         return tmpList
 
+
+test = Doubly_linked_chain()
+print(test.isEmpty())
+test.insert(1)
+print(test.isEmpty())
+
+test.insert(2)
+test.insert(6)
+print(test.getNode(6))
+test.remove(2)
+print(test.getNode(6))
+print(test.getNode(2))
+print(test.getItem(6))
+print(test.getItem(2))
+print(test.traverse())
